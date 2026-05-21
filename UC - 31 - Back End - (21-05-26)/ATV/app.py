e2f7becfea708c291eac55f1990394a9033f4db5
Vitor Emanuel <Vitor00180066100@gmail.com>
2026-05-21 08:46:43 -0300

    Add files via upload

---
+++ UC - 31 - Back End - (21-05-26)/ATV/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get('usuario')
+    senha = request.args.get('senha')
+    cidade = request.args.get('cidade')
+    return render_template('autenticar.html', usuario=usuario, senha=senha, cidade=cidade)
+
+if __name__ == '__main__':
+    app.run(debug=True)
